f09de55182c660eccff97602730a82f02ed02ca0
sentrip <djordje.m.pepic@gmail.com>
2018-08-17 00:41:05 +0200

    Bump version: 0.1.0 → 0.1.1

--- instatools/__init__.py
+++ instatools/__init__.py
@@ -7,7 +7,7 @@ from .instagram import Instagram
 
 __author__ = """Djordje Pepic"""
 __email__ = 'djordje.m.pepic@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = [
     'Action', 'Bot', 'Instagram'
